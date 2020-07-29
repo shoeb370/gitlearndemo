205de7ea00ec10bb12517caf7e35b441b29d1e19
Shoeb Ahmed <shoebahmed370@gmail.com>
2020-07-30 02:16:56 +0530

    Updated in branch

--- for_demo.py
+++ for_demo.py
@@ -3,3 +3,5 @@ for i in range(10):
 print("done with for loop")
 print("Wait what about while loop")
 print("updated author name")
+print("re do again for branch and master based")
+print("{{}}{{{}}}")
